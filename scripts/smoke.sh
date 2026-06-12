fb52f5b49f2a8024b11d580ed0e5c5ad026519fd
tim apple <tim@tims-Air.attlocal.net>
2026-06-12 06:27:17 -0700

    Add github workflow to test on linux

--- scripts/smoke.sh
+++ scripts/smoke.sh
@@ -6,12 +6,14 @@ cd "$(dirname "$0")/.."
 DORA_DOCS_CHECK=1 \
 NVIM_LOG_FILE="${TMPDIR:-/tmp}/dora-nvim.log" \
 nvim --headless -u NONE -i NONE --noplugin \
+  -c "set noswapfile" \
   -c "set rtp^=$PWD" \
   -c "luafile scripts/docs.lua" \
   -c "qa"
 
 NVIM_LOG_FILE="${TMPDIR:-/tmp}/dora-nvim.log" \
 nvim --headless -u NONE -i NONE --noplugin \
+  -c "set noswapfile" \
   -c "set rtp^=$PWD" \
   -c "runtime plugin/dora.lua" \
   -c "luafile scripts/smoke.lua" \
